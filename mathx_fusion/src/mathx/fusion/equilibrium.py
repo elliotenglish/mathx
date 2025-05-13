fix basis calculation, normal calc

mathx_fusion/src/mathx/fusion/equilibrium.py
@@ -106,7 +106,7 @@ def get_xyz_basis(eq,u):
                        r["Z"][:,None]],
                        axis=1)
   basis=jnp.concatenate([r["X_r"][:,None],r["X_t"][:,None],r["X_z"][:,None],
-                         r["Z_r"][:,None],r["Y_t"][:,None],r["Y_z"][:,None],
+                         r["Y_r"][:,None],r["Y_t"][:,None],r["Y_z"][:,None],
                          r["Z_r"][:,None],r["Z_t"][:,None],r["Z_z"][:,None]],
                          axis=1).reshape((-1,3,3))
   # print(rtz)

mathx_fusion/src/mathx/fusion/reactor.py
@@ -43,7 +43,6 @@ class Reactor:
       # log.info(f"{u=}")
       us=u[None,:] if len(u.shape)==1 else u
       # log.info(f"{us=}")
-      us=us+jnp.array([[0,0,1]])
       # log.info(f"computing vertices num={us.shape[0]}")
       xs,bs=equilibrium.get_xyz_basis(self.plasma_equilibrium,us)
       # log.info(f"computed")
@@ -51,11 +50,18 @@ class Reactor:
       # log.info(f"{bs=}")
       xs,bs=(xs[0],bs[0]) if len(u.shape)==1 else (xs,bs)
       return xs,bs
+
+    u=jnp.array([u[0],u[1],1])
+
     x,b=jax.pure_callback(structure_fn_plasma_callback,
                           (jax.ShapeDtypeStruct((3,),u.dtype),
                            jax.ShapeDtypeStruct((3,3),u.dtype)),
                           u,
                           vmap_method="expand_dims")
+    n=jnp.cross(b[:,0],b[:,1])
+    n=n/jnp.linalg.norm(n)
+    x=x+u[2]*n
+
     return x,b
 
   def __init__(self, params: ReactorParameters):
@@ -85,6 +91,8 @@ class Reactor:
       lambda u: self.structure_fn_plasma(jnp.array([u[0],u[1],u[2]*self.wall_thickness]))[0],
       closed=(True,True,False),
       min_segments=(4,4,1))
+
+    # self.magnets=
 
     self.density=128
 
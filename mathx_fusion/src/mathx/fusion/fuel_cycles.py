b7aa9d75c33a948b96b4f57f0b9cbce090e4c67f
Elliot English <elliotenglish@gmail.com>
2025-07-06 17:14:09 +0100

    more reactivity code

--- mathx_fusion/src/mathx/fusion/fuel_cycles.py
+++ mathx_fusion/src/mathx/fusion/fuel_cycles.py
@@ -1,4 +1,5 @@
 import jax.numpy as jnp
+import jax
 import json
 import scipy
 
@@ -24,15 +25,36 @@ def get_data():
     with files("mathx.fusion").joinpath("fuel_cycles.json").open("r") as f:
       data=json.load(f)
   return data
-    
+
+def keV(T):
+  return T/ev_in_K*1e-3
+
 class Fuel:
   def __init__(self,name):
     self.fuel_data=[d for d in get_data() if d["name"]==name][0]
 
   def cross_section(self,T):
     if "duane_coefficients" in self.fuel_data:
-      E=T/ev_in_K*1e-3
+      E=keV(T)
       return duane_function(E,self.fuel_data["duane_coefficients"])
     raise ValueError
+  
+  def reactivity(self,T):
+    """
+    For a given T this is the Maxwellian average velocity-cross section product.
+    """
+
+    Ts=(0,2e9,100)
+    
+    Es=keV(T)
+    ss=jax.vmap(self.cross_section)(Es)
+    
+    dT=Ts[1]-Ts[0]
+
+    mu=m1*m2/(m1+m2)
+
+    reactivity=4/(2*jnp.pi*m1)**.5 * (mu/(m1*k*T))**1.5 * jnp.sum(Es*ss*jnp.exp(mu*Es/(m1*k*T)*dT)
+                                                                  
+                                                                  return reactivity
 
   # def velocity(self,T,rho):
